bf99332576b42feb53539c2f33d196a80a01e0a9
NelsonRyan-06 <nelsonarabit00@gmail.com>
2023-06-07 18:46:58 +0800

    remove router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import Navbar from "./components/Navbar.vue";
+import Index from "./views/Index.vue";
 </script>
 
 <template>
-  <header>
-    <Navbar />
-  </header>
-
-  <RouterView />
+  <Index />
 </template>
-
-<style lang="css" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,10 +3,12 @@ import Home from "@/components/Home.vue";
 import About from "../components/About.vue";
 import Project from "@/components/Project.vue";
 import Social from "@/components/Social.vue";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <div class="mx-auto pb-32">
+  <div class="mx-auto pb-10">
+    <Navbar />
     <Home />
     <About />
     <Project />
