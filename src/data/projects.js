32890197e9ecee30fdf80fa2c441d8a8515f35e2
Nelson Ryan <nelsonarabit00@gmail.com>
2023-07-28 16:27:58 +0800

    Update Music APP Project

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,6 +9,7 @@ import Lazhapee from "../assets/images/Lazhapee.png";
 import Laragigs from "../assets/images/Laragigs.png";
 import Conversion from "../assets/images/Conversion.png";
 import FlowerStore from "../assets/images/FlowerStore.png";
+import MusicApp from "../assets/images/Music-App.png";
 
 export default [
   {
@@ -24,6 +25,47 @@ export default [
   },
   {
     id: 2,
+    title: "Music App",
+    description:
+      "It is an application wherein you can store songs, youtube video and post, And view other's post in Posts Section",
+    technologies: [
+      "Vue",
+      "JavaScript",
+      "Tailwind CSS",
+      "Laravel",
+      "Rest API",
+      "Pinia",
+      "MySQL",
+      "Postman",
+    ],
+    repository: "https://github.com/nlsnryn/Music-Project.UI",
+    image: MusicApp,
+    demo: "https://nlsn-music-app.netlify.app/",
+    api: "https://github.com/nlsnryn/Music-Project.API",
+  },
+  {
+    id: 3,
+    title: "FlowerStore",
+    description:
+      "A fullstack e-commerce website. This is my Fullstack Developer Exam in FlowerStorePH Company.",
+    technologies: [
+      "Vue",
+      "JavaScript",
+      "Tailwind CSS",
+      "Laravel API",
+      "Rest API",
+      "Pinia",
+      "MySQL",
+      "Postman",
+      "Axios",
+    ],
+    repository: "https://github.com/nlsnryn/flowerstore.UI",
+    image: FlowerStore,
+    demo: "https://flowerstore-project.netlify.app/",
+    api: "https://github.com/nlsnryn/flowerstore.API",
+  },
+  {
+    id: 4,
     title: "Skillable",
     description:
       "It is an application wherein you can create account and login. You can also store, update and delete some notes or skills",
@@ -43,7 +85,7 @@ export default [
     api: "https://github.com/nlsnryn/Skillable.API",
   },
   {
-    id: 3,
+    id: 5,
     title: "Lazhapee",
     description: "E-commerce website created using Vue JS and Tailwind CSS",
     technologies: [
@@ -60,7 +102,7 @@ export default [
     api: "https://fakestoreapi.com/",
   },
   {
-    id: 4,
+    id: 6,
     title: "WeatherRN",
     description:
       "A webapp for tracking city or state weather real time using fahreinheit",
@@ -77,7 +119,7 @@ export default [
     api: false,
   },
   {
-    id: 5,
+    id: 7,
     title: "QuizApp",
     description:
       "Quiz app that you can answer some questions and getting score once done",
@@ -88,7 +130,7 @@ export default [
     api: false,
   },
   {
-    id: 6,
+    id: 8,
     title: "I need to do",
     description: "A vue application to do notes",
     technologies: ["Vue", "Tailwind CSS", "Pinia"],
@@ -98,7 +140,7 @@ export default [
     api: false,
   },
   {
-    id: 7,
+    id: 9,
     title: "VueNotes",
     description: "A vue application that you can store some notes",
     technologies: ["Vue", "CSS"],
@@ -108,7 +150,7 @@ export default [
     api: false,
   },
   {
-    id: 8,
+    id: 10,
     title: "Memes Generator",
     description:
       "A react application that generate some memes picture and can input custom text.",
@@ -119,7 +161,7 @@ export default [
     api: false,
   },
   {
-    id: 9,
+    id: 11,
     title: "Laragigs",
     description: "Laravel application with authentication and job listings.",
     technologies: ["Laravel", "Tailwind CSS", "Alpine JS"],
@@ -129,7 +171,7 @@ export default [
     api: false,
   },
   {
-    id: 10,
+    id: 12,
     title: "Converstion Number to Words",
     description:
       "Laravel application that can convert number to words and vice versa.",
@@ -138,26 +180,5 @@ export default [
     image: Conversion,
     demo: false,
     api: false,
-  },
-  {
-    id: 11,
-    title: "FlowerStore",
-    description:
-      "A fullstack e-commerce website. This is my Fullstack Developer Exam in FlowerStorePH Company.",
-    technologies: [
-      "Vue",
-      "JavaScript",
-      "Tailwind CSS",
-      "Laravel API",
-      "Rest API",
-      "Pinia",
-      "MySQL",
-      "Postman",
-      "Axios",
-    ],
-    repository: "https://github.com/nlsnryn/flowerstore.UI",
-    image: FlowerStore,
-    demo: "https://flowerstore-project.netlify.app/",
-    api: "https://github.com/nlsnryn/flowerstore.API",
   },
 ];
